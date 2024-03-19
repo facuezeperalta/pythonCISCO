ed2cab95a0786d458675e3e6786d4dab36ce8f07
Facundo Peralta <facundoperaltaezequiel@gmail.com>
2024-03-19 18:22:51 -0300

    primer commit

---
+++ literales.py
@@ -0,0 +1,6 @@
+print(0o123) #octal
+print(0x123) #hexadecimal
+
+print("I´m\nlearning\nPython")
+print(30/13)
+print(30//13)   
---
+++ primerPrograma.py
@@ -0,0 +1,11 @@
+print("Hisssss.......")
+
+print("Hola mundo!")
+
+print("La Witsi Witsi Araña\nsubió a su telaraña.")
+print()
+print("Vino la lluvia\ny se la llevó.")
+
+
+print("Hola mi nombre es ", "Facundo Ezequiel Peralta", end=" - ")
+print("Fotógrafo")
